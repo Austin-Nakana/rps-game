771ef83ed1305ee3cc2d1bc5e40be28a30525814
Austin-Nakana <austinnakana@gmail.com>
2026-01-09 15:13:26 +0200

    feat(RNG): Add a function to generate random numbers

--- game.js
+++ game.js
@@ -1,6 +1,21 @@
+//choice enum
 const choice = {
     ROCK: "Rock",
     PAPER: "Paper",
     SCISSORS: "Scissors"
+}
+
+//Random number generation with both bounds inclusive
+function genRandomNum(lowerBound, upperBound){
+    //handle float bounds 
+    lowerBound = Math.ceil(lowerBound);
+    upperBound = Math.floor(upperBound);
+    
+    let rangeSize = (upperBound - lowerBound) + 1; //+1 makes upperBound reachable(inclusive)
+    
+    //Math.floor() to remove decimals
+    let randomNum = Math.floor(Math.random() * rangeSize) + lowerBound;// + lowerBound makes range start at lowerBound
+
+    return randomNum; // lowerBound <= randomNum <= upperBound
 }
 
